6c32f982af20ca195214a6473947e3af7b9141a0
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-09-04 01:19:14 +0800

    create Profile and fix bundle

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,16 @@
 class StaticPagesController < ApplicationController
-	def home
-		
-	end
+  def home
+    
+  end
 
-	def help
-		
-	end
-	def about
-		
-	end
-	def contact
-		
-	end
-	
+  def help
+    
+  end
+  def about
+    
+  end
+  def contact
+    
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_100723) do
+ActiveRecord::Schema.define(version: 2018_09_03_170148) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
